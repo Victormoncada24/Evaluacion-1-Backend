e5b81746bc724b6a190c66296c0f128837fa68c2
Manix <victor.moncada001@gmail.com>
2026-09-02 18:17:29 -0400

    ultimo ajustes

--- academic_project/academic/admin.py
+++ academic_project/academic/admin.py
@@ -1,3 +1,9 @@
+# academic/admin.py
 from django.contrib import admin
+from .models import Teacher, Course, Student, StudentCourse
 
-# Register your models here.
+# Registrar cada modelo para que aparezca en el panel de administración
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(StudentCourse)
--- academic_project/academic/models.py
+++ academic_project/academic/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# Modelo Teacher: representa a los docentes de la institución
 class Teacher(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
